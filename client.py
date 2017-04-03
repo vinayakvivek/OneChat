9e9215f68c1b5a30e02c0796b8084c41122a27c1
vinayak <vinayakdeepu@gmail.com>
2017-04-03 22:01:42 +0530

    File transfer from server to client

--- client.py
+++ client.py
@@ -1,10 +1,12 @@
 import socket
 
+buffer_size = 100
+
 s = socket.socket(socket.AF_INET, 
 				  socket.SOCK_STREAM)
 
 host = 'localhost'
-port = 51713
+port = 51714
 
 # ip = socket.inet_aton('127.0.0.2')
 # print socket.inet_ntoa(ip)
@@ -12,8 +14,18 @@ port = 51713
 s.bind(('127.0.0.1', 32345))
 s.connect((host, port))
 
-msg = s.recv(1024)
+# s.send('Hi pls send me the file!')
 
+file_data = ''
+
+while True:
+	data = s.recv(buffer_size)
+	if not data:
+		break
+
+	print('recieved data : %s' % data)
+	file_data += data
+
+
+print(file_data)
 s.close()
-
-print('server : %s' % (msg))
--- server.py
+++ server.py
@@ -1,11 +1,13 @@
 import socket
 import time
 
+buffer_size = 100
+
 server_socket = socket.socket(socket.AF_INET, 
 							  socket.SOCK_STREAM)
 
 host = '127.0.0.1' # localhost
-port = 51713
+port = 51714
 
 # bind to the port
 server_socket.bind((host, port))
@@ -15,8 +17,19 @@ print('waiting for connection...')
 
 while True:
 	client_socket, addr = server_socket.accept();
-	# print type(addr)
-	print addr
-	# print("Got a connection from %s" % (socket.inet_ntoa(addr)))
-	client_socket.send("Hi")
+	print("connected to %s" % (str(addr)))
+
+	client_data = client_socket.recv(buffer_size)
+	print('Data recieved by the server : %s' % (str(client_data)))
+
+	f = open('random.txt', 'rb')
+	line = f.read(buffer_size)
+	while (line):
+		client_socket.send(line)
+		print('sent ', line)
+		line = f.read(buffer_size)
+
+	f.close()
+
+	client_socket.send('connection terminating..')
 	client_socket.close()
